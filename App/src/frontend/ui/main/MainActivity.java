6fa15d3dd41d3ea7384d577033346009dc4efeba
Steven Fraim <stevenfraim@gmail.com>
2011-10-21 22:16:13 -0700

    .

--- App/src/frontend/ui/main/MainActivity.java
+++ App/src/frontend/ui/main/MainActivity.java
@@ -2,7 +2,7 @@ package frontend.ui.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.google.common.collect.ImmutableSet;
+import org.apache.commons.logging.Log;
 
 public class MainActivity extends Activity
 {
@@ -11,13 +11,6 @@ public class MainActivity extends Activity
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-
-    ImmutableSet.of("Test to make sure you all have Guava setup");
-    new TestObject().testMethod("Test to make sure you have EasyMock setup");
-  }
-
-  // TODO(sfraim) remove this after everyone gets their project setup.
-  public static class TestObject {
-    public void testMethod(String testMethodInput) { }
+    android.util.Log.e("PENIS", "Penis");
   }
 }
