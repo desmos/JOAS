372454b4099141cbff79947117761d8de2167103
Steven Fraim <stevenfraim@gmail.com>
2011-10-22 12:46:22 -0700

    Move fate to the gameView and remove main.xml

--- app/gen/frontend/ui/main/R.java
+++ app/gen/frontend/ui/main/R.java
@@ -10,21 +10,16 @@ package frontend.ui.main;
 public final class R {
     public static final class attr {
     }
-    public static final class id {
-        public static final int gameView=0x7f040001;
-        public static final int widget0=0x7f040000;
-    }
-    public static final class layout {
-        public static final int game=0x7f020000;
-        public static final int main=0x7f020001;
     public static final class drawable {
         public static final int fate=0x7f020000;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int gameView=0x7f050001;
+        public static final int imageView1=0x7f050002;
+        public static final int widget0=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int game=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
